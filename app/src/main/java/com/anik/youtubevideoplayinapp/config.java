53b7097ae63fea991b6fe96e0fc1b7160179c4af
Noor Mohammed Anik <noormohammedanik@gmail.com>
2018-03-05 12:41:06 +0600

    Initial commit

---
+++ app/src/main/java/com/anik/youtubevideoplayinapp/config.java
@@ -0,0 +1,14 @@
+package com.anik.youtubevideoplayinapp;
+
+/**
+ * Created by Noor Mohammed on 05-Mar-2018.
+ */
+
+public final class config {
+
+    private config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyCTi9zsCNvdAJzsohhvXPJf_zA2Rep4NO4";
+
+}
